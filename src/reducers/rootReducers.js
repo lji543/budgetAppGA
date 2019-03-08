fcfa0d73b2461395abff5f57ceb2bf2803dd39ab
Illig, Laura EX1 <lillig@us.imshealth.com>
2019-03-08 10:04:44 -0800

    first-w errors

---
+++ src/reducers/rootReducers.js
@@ -0,0 +1,15 @@
+import { combineReducers } from 'redux';
+
+import profile from './profileReducer';
+import expenses from './expenseReducer';
+
+const rootReducer = combineReducers({
+  list
+});
+
+export default rootReducer;
+
+
+// Home
+// Profile
+// Expenses

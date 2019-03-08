3566e3f3ae00059cbaaec0f417eeb9faa7471354
Cook, Scott EX1 <scott.cook@us.imshealth.com>
2019-03-08 10:56:50 -0800

    Update state to reflect budget and username

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,13 +3,48 @@ import { connect } from 'react-redux';
 import * as profileActions from '../actions/profileActions';
 
 class Profile extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: 'Michael Scott',
+      budget: 10
+    }
+  }
 
   render() {
+
     return (
-      <div className="">
-        Profile
+      <div >
+        <h2>Hey {this.state.name}!</h2>
+        <h3>You have an income of {this.state.budget} dollars.</h3>
+        <p>(you could use that to open a lemonade stand)</p>
+
+
+        <h3>Update Your Profile</h3>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            value={this.state.name}
+            onChange={ e => this.setState({name:e.target.value})}
+          />
+          <input type="submit"
+            value="Update"
+          />
+        </form>
+
+        <h3>Update Your Budget</h3>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            value={this.state.budget}
+            onChange={ e => this.setState({budget:e.target.value})}
+          />
+          <input type="submit"
+            value="Update"
+          />
+        </form>
+
       </div>
-    );
+    )
   }
 }
 
